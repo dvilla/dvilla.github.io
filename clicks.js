47611bb7fe92e2b8d281e505c629539cc7f3ebb8
Daniel Villa Rivera <danielvilla@1984.local>
2020-04-05 19:11:33 -0700

    added auto calculation of current job months

--- clicks.js
+++ clicks.js
@@ -14,6 +14,29 @@ function changeColor(){
 function init(){
   var firstName = document.getElementById("tname");
   firstName.onclick = changeColor;
+  getCurrentJobTime();
+}
+
+function getCurrentJobTime(){
+  var current_job = document.getElementById("current_job");
+  var initial_date = new Date(2020, 1, 22);
+  var date_now = new Date(Date.now());
+  var initial_date_month_and_year = getMonthAndYear(initial_date);
+  var date_now_month_and_year = getMonthAndYear(date_now);
+
+  var total_months = date_now.getFullYear() * 12;
+  total_months -= initial_date.getFullYear() * 12;
+  total_months += date_now.getMonth();
+  total_months -= initial_date.getMonth();
+
+  current_job.innerText = initial_date_month_and_year +  " – " + date_now_month_and_year + " ~ " + total_months + " mos";
+}
+
+function getMonthAndYear(date){
+  const monthNames = ["Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  var monthName = monthNames[date.getMonth()];
+  var year = date_now_year = date.getFullYear();
+  return monthName + " " + year;
 }
 
 window.onload = init;
