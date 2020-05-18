4d1c1a12d90aa5a7c7c10a7787c2a425abd2fe88
Daniel Villa Rivera <danielvilla@1984.local>
2020-05-17 21:20:00 -0700

    added state flags for every state

--- capitalGuessMexico/guessTheCapital.js
+++ capitalGuessMexico/guessTheCapital.js
@@ -1,37 +1,37 @@
-window.onload = init;
+window.onload = initiateGame;
 
 let statesAndCapitals = [
-  ["MEX2706", "Mexicali"],
-  ["MEX2707", "La Paz"],
-  ["MEX2708", "Saltillo"],
-  ["MEX2709", "Chihuahua"],
-  ["MEX2710", "Victoria de Durango"],
-  ["MEX2711", "Culiacan Rosales"],
-  ["MEX2712", "Hermosillo"],
-  ["MEX2713", "Zacatecas"],
-  ["MEX2714", "Monterrey"],
-  ["MEX2715", "San Luis Potosi"],
-  ["MEX2716", "Ciudad Victoria"],
-  ["MEX2717", "Aguascalientes"],
-  ["MEX2718", "Colima"],
-  ["MEX2719", "Guadalajara"],
-  ["MEX2720", "Morelia"],
-  ["MEX2721", "Tepic"],
-  ["MEX2722", "San Francisco de Campeche"],
-  ["MEX2723", "Oaxaca de Juarez"],
-  ["MEX2724", "Puebla de Zaragoza"],
-  ["MEX2725", "Villahermosa"],
-  ["MEX2726", "Tlaxcala"],
-  ["MEX2728", "Guanajuato"],
-  ["MEX2729", "Chilpancingo de los Bravos"],
-  ["MEX2730", "Pachuca de Soto"],
-  ["MEX2731", "Toluca de Lerdo"],
-  ["MEX2732", "Cuernavaca"],
-  ["MEX2733", "Santiago de Queretaro"],
-  ["MEX2734", "Xalapa-Enriquez"],
-  ["MEX2735", "Tuxtla Gutierrez"],
-  ["MEX2736", "Chetumal"],
-  ["MEX2737", "Merida"]
+  ["MEX2706", "Mexicali", "840px 480px"],
+  ["MEX2707", "La Paz", "720px 480px"],
+  ["MEX2708", "Saltillo", "480px 480px"],
+  ["MEX2709", "Chihuahua", "120px 480px"],
+  ["MEX2710", "Victoria de Durango", "840px 360px"],
+  ["MEX2711", "Culiacan Rosales", "960px 120px"],
+  ["MEX2712", "Hermosillo", "840px 118px"],
+  ["MEX2713", "Zacatecas", "120px 120px"],
+  ["MEX2714", "Monterrey", "720px 240px"],
+  ["MEX2715", "San Luis Potosi", "120px 240px"],
+  ["MEX2716", "Ciudad Victoria", "600px 120px"],
+  ["MEX2717", "Aguascalientes", "960px 480px"],
+  ["MEX2718", "Colima", "360px 480px"],
+  ["MEX2719", "Guadalajara", "360px 360px"],
+  ["MEX2720", "Morelia", "120px 360px"],
+  ["MEX2721", "Tepic", "840px 238px"],
+  ["MEX2722", "San Francisco de Campeche", "600px 480px"],
+  ["MEX2723", "Oaxaca de Juarez", "600px 240px"],
+  ["MEX2724", "Puebla de Zaragoza", "480px 240px"],
+  ["MEX2725", "Villahermosa", "720px 120px"],
+  ["MEX2726", "Tlaxcala de Xicohtencatl", "480px 119px"],
+  ["MEX2728", "Guanajuato", "720px 360px"],
+  ["MEX2729", "Chilpancingo de los Bravos", "600px 360px"],
+  ["MEX2730", "Pachuca de Soto", "480px 360px"],
+  ["MEX2731", "Toluca de Lerdo", "240px 360px"],
+  ["MEX2732", "Cuernavaca", "960px 238px"],
+  ["MEX2733", "Santiago de Queretaro", "360px 240px"],
+  ["MEX2734", "Xalapa-Enriquez", "360px 120px"],
+  ["MEX2735", "Tuxtla Gutierrez", "240px 480px"],
+  ["MEX2736", "Chetumal", "240px 240px"],
+  ["MEX2737", "Merida", "240px 120px"],
 ];
 
 let mistakes = 0;
@@ -42,13 +42,15 @@ let stateMapElementName;
 let stateTextElement;
 let userAnswer;
 let answer;
+let stateFlag;
 
-function init() {
-  initiateGame();
+function initiateGame() {
+  setNewState();
   addKeyListener();
 }
 
-function initiateGame() {
+function setNewState() {
+  stateFlag = document.getElementById("stateFlag");
   randomNumber = Math.floor(Math.random() * (statesAndCapitals.length));
   randomState = statesAndCapitals[randomNumber];
   stateMapElement = document.getElementById(randomState[0]);
@@ -57,6 +59,7 @@ function initiateGame() {
   stateTextElement.textContent = stateMapElementName + "?";
 
   stateMapElement.style = "fill: blue";
+  stateFlag.style = `background-position: ${randomState[2]}`;
 
   userAnswer = hideCapitalAnswer(randomState[1]);
   answer = randomState[1].toLowerCase().split("");
@@ -66,7 +69,8 @@ function initiateGame() {
 
 function addKeyListener() {
   document.addEventListener('keyup', (e) => {
-    let indexOfLetter = answer.indexOf(e.key);
+    let userInput = e.key.toLowerCase();
+    let indexOfLetter = answer.indexOf(userInput);
     
     if(statesAndCapitals.length === 0){
       document.getElementById("capitalAnswer").textContent = "YOU WON!";
@@ -74,7 +78,7 @@ function addKeyListener() {
     }
 
     if(indexOfLetter != -1){
-      replaceLetterInDisplayedAnswer(answer, e.key, userAnswer);
+      replaceLetterInDisplayedAnswer(answer, userInput, userAnswer);
       document.getElementById("capitalAnswer").textContent = userAnswer.join("");
       isWordCompleted(userAnswer)
     } else {
@@ -99,6 +103,6 @@ function isWordCompleted(userAnswer) {
   if(userAnswer.indexOf("_") === -1){
     stateMapElement.style = "fill: green";
     statesAndCapitals.splice(randomNumber, 1)
-    initiateGame();
+    setNewState();
   }
 }
